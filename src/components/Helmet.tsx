0214ffd3264bedd1f2dc9bd307198ab93da41ded
Asad Ghumman <asadghumman195@gmail.com>
2020-12-21 13:42:24 +0500

    asd

--- src/components/Helmet.tsx
+++ src/components/Helmet.tsx
@@ -7,7 +7,7 @@ export const HelmetPage: React.FC = () => {
         <div>
             <Helmet>
 
-                <title>GamicaCLOUDasdasdasdasdsdasdasdasdsdadasdss</title>
+                <title>GamicaCLOUDasdasdasdasdsdasdasdasdsdadasdsssdfsdfsdfsdfsdfsdfsdfsd</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossOrigin="anonymous" />
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossOrigin="anonymous" />
